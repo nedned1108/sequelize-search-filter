5600a4d4434b83268c536382d61da6a747bb5133
nednguyen <rpmshockwave@gmail.com>
2022-12-16 19:16:00 -0500

    finished

--- routes/players.js
+++ routes/players.js
@@ -4,11 +4,24 @@ const router = express.Router();
 const { Player } = require('../db/models');
 
 router.get('/', async (req, res) => {
-  let { firstName, number } = req.query;
+  let { firstName, number, page, size } = req.query;
+  const query = {}
+
+  page = page ? +page : 1;
+  size = size ? +size : 2;
+
+  if (size > 10) {
+    size = 10;
+  }
+
+  if (page > 0 && size > 0 && size <= 10) {
+    query.limit = size;
+    query.offset = size * (page - 1)
+  }
 
   const where = {};
   if (firstName && firstName !== '') {
-    where.firstName = firstName[0].toLowerCase() + firstName.slice(1);
+    where.firstName = firstName[0].toUpperCase() + firstName.slice(1);
   } else if (firstName === '') {
     res.status(400);
     return res.json({
@@ -19,7 +32,7 @@ router.get('/', async (req, res) => {
   }
 
   if (number) {
-    if (!isNaN(number) && number <= 0) {
+    if (!Number.isNaN(number) && number >= 0) {
       where.number = parseInt(number);
     } else {
       res.status(400);
@@ -33,9 +46,12 @@ router.get('/', async (req, res) => {
 
   const players = await Player.findAll({
     where,
+    ...query
   });
   return res.json({
     players,
+    page,
+    size
   });
 });
 
